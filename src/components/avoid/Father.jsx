3af09110f7d0c258330a85128a4e1408896b1d8b
equiman <equiman@users.noreply.github.com>
2021-10-03 22:17:11 -0500

    Fix father

--- src/components/avoid/Father.jsx
+++ src/components/avoid/Father.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Father = ({ className, name, lastName}) => {
   return (
-      <div className={className}>
-        {name}
-        {!!lastName && (
-          <span> {lastName}</span>
-        )}
-      </div>
+    <div className={className}>
+      {name}
+      {!!lastName && (
+        <span> {lastName}</span>
+      )}
+    </div>
   );
 };
 
